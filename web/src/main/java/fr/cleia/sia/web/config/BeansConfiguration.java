2845ed77f9b8039999dcc43f040227d9baa8766c
aurelien barthelemy <aurelien.barthelemy.ab@gmail.com>
2025-10-02 23:14:01 +0200

    CLE-13 Mettre en place une pipeline CI GitHub Actions pour exécuter les tests Maven

--- web/src/main/java/fr/cleia/sia/web/config/BeansConfiguration.java
+++ web/src/main/java/fr/cleia/sia/web/config/BeansConfiguration.java
@@ -1,6 +1,8 @@
 package fr.cleia.sia.web.config;
 
 import fr.cleia.sia.application.ports.DepotDeFonds;
+import fr.cleia.sia.application.usecase.ConsulterArborescence;
+import fr.cleia.sia.application.usecase.ConsulterArborescenceService;
 import fr.cleia.sia.application.usecase.CreerArborescence;
 import fr.cleia.sia.application.usecase.CreerArborescenceService;
 import fr.cleia.sia.domain.description.rules.PolitiqueDeDescription;
@@ -24,6 +26,10 @@ public class BeansConfiguration
     @Bean
     CreerArborescence creerArborescence (DepotDeFonds depotDeFonds, PolitiqueDeDescription politique) {
         return new CreerArborescenceService(depotDeFonds, politique);
+    }
 
-}
+    @Bean
+    ConsulterArborescence consulterArborescence (DepotDeFonds depotDeFonds) {
+        return new ConsulterArborescenceService(depotDeFonds);
+    }
 }
